289683d16e012504946856d55012dc3847685253
안형노 <hrahn@estsecurity.com>
2022-03-02 10:20:21 +0900

    api/products 구현

---
+++ pages/api/products/index.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/client";
+import { withApiSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    body: { name, price, description },
+    session: { user },
+  } = req;
+
+  const product = await client.product.create({
+    data: {
+      name,
+      price: +price,
+      description,
+      image: "xx",
+      user: {
+        connect: {
+          id: user?.id,
+        },
+      },
+    },
+  });
+  console.log("product", product);
+
+  res.json({
+    ok: true,
+    product,
+  });
+}
+
+export default withApiSession(
+  withHandler({
+    method: "POST",
+    handler,
+  })
+);
